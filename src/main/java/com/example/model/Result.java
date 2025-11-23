48399caa1e447502f369bc2c2555cce060da2fac
Yajoshi <joshiyashmca@gmail.com>
2025-11-22 22:27:50 -0500

    added
    added files

---
+++ src/main/java/com/example/model/Result.java
@@ -0,0 +1,19 @@
+package com.example.model;
+
+public class Result {
+    private String message;
+
+    public Result() { }
+
+    public Result(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    @Override
+    public String toString() {
+        return "Result{message='" + message + "'}";
+    }
+}
